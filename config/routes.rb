aa185fbb663b535bac0546476d1998c6594f5bb8
MarilenaRoque <marilenas@id.uff.br>
2021-09-05 11:34:57 -0300

    Add sidekiq configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reports, only: [:index, :create]
+
+  ### Creates a route to sidekiq panel
+  ### Shouldn't be added on production
+  mount Sidekiq::Web => '/sidekiq'
+  # For details on the DSL a vailable within this file, see https://guides.rubyonrails.org/routing.html
 end
